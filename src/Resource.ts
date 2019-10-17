c0e2953d577440dca45b849b4eabadddbaaf00b5
corno <corno@schraverus.com>
2019-10-17 14:00:23 +0200

    changed types to interfaces

--- src/Resource.ts
+++ src/Resource.ts
@@ -17,7 +17,7 @@ export interface IUnsafeOpenableResource<OpenedResource, OpenError> {
 }
 
 
-export type ISafeResource<ResourceType> = ISafeOpenableResource<ISafeOpenedResource<ResourceType>>
-export type IUnsafeResource<ResourceType, OpenError, CloseError> = IUnsafeOpenableResource<IUnsafeOpenedResource<ResourceType, CloseError>, OpenError>
-export type IUnsafeOnOpenResource<ResourceType, OpenError> = IUnsafeOpenableResource<ISafeOpenedResource<ResourceType>, OpenError>
-export type IUnsafeOnCloseResource<ResourceType, CloseError> = ISafeOpenableResource<IUnsafeOpenedResource<ResourceType, CloseError>>
+export interface ISafeResource<ResourceType> extends ISafeOpenableResource<ISafeOpenedResource<ResourceType>> {}
+export interface IUnsafeResource<ResourceType, OpenError, CloseError> extends IUnsafeOpenableResource<IUnsafeOpenedResource<ResourceType, CloseError>, OpenError> {}
+export interface IUnsafeOnOpenResource<ResourceType, OpenError> extends IUnsafeOpenableResource<ISafeOpenedResource<ResourceType>, OpenError> {}
+export interface IUnsafeOnCloseResource<ResourceType, CloseError> extends ISafeOpenableResource<IUnsafeOpenedResource<ResourceType, CloseError>> {}
