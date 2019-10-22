d2207fd9cad837bdf61fbdba555aa13a256c026c
corno <corno@schraverus.com>
2019-10-22 09:14:03 +0200

    added limiting functionality and added comments

--- src/Stream.ts
+++ src/Stream.ts
@@ -1,3 +1,24 @@
+export type Limiter = {
+    /**
+     * maximum amount of times onData is called
+     */
+    maximum: number,
+    /**
+     * if the provider knows that the amount of records exceeds 'maximum', it can choose to send even less records, even 0.
+     */
+    abortEarly: boolean
+}
+
+/**
+ * a minimalistic interface that supports streaming
+ */
 export interface IStream<Data> {
-    process(onData: (data: Data) => void, onEnd: () => void): void;
+    /**
+     * 
+     * @param limiter the limiter is a hint to the stream provider to limit the amount of times onData is called.
+     * @param onData callback for a data element, the second argument (abort) requests the provider to abort the stream. This is not quaranteed
+     * @param onEnd callback that will be called when the stream is finished. aborted will be set to true if not the full dataset is received. This will always be caused by the caller
+     * either by setting the limiter or by calling the abort function on onData
+     */
+    process(limiter: null | Limiter, onData: (data: Data, abort: () => void) => void, onEnd: (aborted: boolean) => void): void;
 }
