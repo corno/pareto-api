79f1762fdd197481ce59b431eee500d8bc8c0963
corno <corno@schraverus.com>
2019-10-24 11:12:51 +0200

    removed default libs and added a minimal lib file to satisfy the tsc compiler

---
+++ src/lib.d.ts
@@ -0,0 +1,11 @@
+// tslint:disable: interface-name no-empty-interface
+interface Array<T> {}
+interface Boolean {}
+interface CallableFunction {}
+interface Function {}
+interface IArguments {}
+interface NewableFunction {}
+interface Number {}
+interface Object {}
+interface RegExp {}
+interface String {}
