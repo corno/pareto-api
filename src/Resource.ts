b95ce7a0790b62d777ba22918bb564bf0fa3c2c0
corno <gitkraken.a@schraverus.com>
2020-10-13 07:29:58 +0200

    renamed members

--- src/Resource.ts
+++ src/Resource.ts
@@ -2,19 +2,19 @@
  * an opened resource that will not have an error on closing
  */
 export interface ISafeOpenedResource<ResourceType> {
-    readonly resource: ResourceType
-    closeSafeOpenedResource(): void
+    readonly content: ResourceType
+    close(): void
 }
 
 /**
  * an opened resource that can have an error on closing
  */
 export interface IUnsafeOpenedResource<ResourceType, CloseError> {
-    readonly resource: ResourceType
+    readonly content: ResourceType
     /**
      * @param onError onError will be called if an error occurred when the resource was closed
      */
-    closeUnsafeOpenedResource(onError: (error: CloseError) => void): void
+    close(onError: (error: CloseError) => void): void
 }
 
 /**
@@ -24,7 +24,7 @@ export interface ISafeOpenableResource<OpenedResource> {
     /**
      * @param onOpened this callback will be called when the resource was opened
      */
-    openSafeOpenableResource(onOpened: (openedResource: OpenedResource) => void): void
+    open(onOpened: (openedResource: OpenedResource) => void): void
 }
 
 /**
@@ -35,5 +35,5 @@ export interface IUnsafeOpenableResource<OpenedResource, OpenError> {
      * @param onError this callback will be called if an error occurred when the resource was opened
      * @param onOpened this callback will be called when the resource was successfully opened
      */
-    openUnsafeOpenableResource(onError: (openError: OpenError) => void, onOpened: (openedResource: OpenedResource) => void): void
+    open(onError: (openError: OpenError) => void, onOpened: (openedResource: OpenedResource) => void): void
 }
